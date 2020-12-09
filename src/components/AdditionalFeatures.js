bc94e86e2240f41c06a2d7e4c3b22a9358d2af4d
sebastian espeset <sebastian.c.espeset@gmail.com>
2020-12-08 20:55:11 -0600

    working on ADD_FEATURE

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
+import { connect } from 'react-redux';
+
 
 const AdditionalFeatures = props => {
   return (
@@ -17,5 +19,11 @@ const AdditionalFeatures = props => {
     </div>
   );
 };
+const mapStateToProps=(state)=>{
+  
+  return{
+    additionalFeatures:state.additionalFeatures
+  };
+}
 
-export default AdditionalFeatures;
+export default connect(mapStateToProps)(AdditionalFeatures);
